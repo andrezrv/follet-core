32dbb52937c2d04a8fc07e66aa274cb54884cb86
andrezrv <andrezrv@gmail.com>
2014-12-09 01:36:43 -0300

    Segment theme support.

--- includes/theme-support.php
+++ includes/theme-support.php
@@ -1,31 +1,36 @@
 <?php
-if ( ! function_exists( 'follet_do_theme_support' ) ) :
-add_action( 'follet_theme_support', 'follet_do_theme_support' );
+if ( ! function_exists( 'follet_theme_support_feed' ) ) :
+add_action( 'follet_theme_support', 'follet_theme_support_feed' );
 /**
- * Initialize default theme support.
+ * Add default posts and comments RSS feed links to head.
  *
- * Default theme support includes:
- *
- * - automatic-feed-links
- * - post-thumbnails
- * - post-formats (all post formats)
- * - custom-background
- * - html5
- * - bootstrap (custom, for Bootstrap)
- * - infinite-scroll (for Jetpack)
- *
- * @uses   add_theme_support()
- * @return void
- * @since  1.0
+ * @since 1.1
  */
-function follet_do_theme_support() {
-	// Add default posts and comments RSS feed links to head.
+function follet_theme_support_feed() {
 	add_theme_support( 'automatic-feed-links' );
+}
+endif;
 
-	// Enable support for Post Thumbnails on posts and pages.
+if ( ! function_exists( 'follet_theme_support_post_thumbnails' ) ) :
+add_action( 'follet_theme_support', 'follet_theme_support_post_thumbnails' );
+/**
+ * Enable support for Post Thumbnails on posts and pages.
+ *
+ * @since 1.1
+ */
+function follet_theme_support_post_thumbnails() {
 	add_theme_support( 'post-thumbnails' );
+}
+endif;
 
-	// Enable support for Post Formats.
+if ( ! function_exists( 'follet_theme_support_post_formats' ) ) :
+add_action( 'follet_theme_support', 'follet_theme_support_post_formats' );
+/**
+ * Enable support for Post Formats.
+ *
+ * @since 1.1
+ */
+function follet_theme_support_post_formats() {
 	add_theme_support(
 		'post-formats', apply_filters( 'follet_post_formats', array(
 				'aside',
@@ -40,8 +45,18 @@ function follet_do_theme_support() {
 			)
 		)
 	);
+}
+endif;
 
-	// Setup the WordPress core custom background feature.
+if ( ! function_exists( 'follet_theme_support_custom_background' ) ) :
+add_action( 'follet_theme_support', 'follet_theme_support_custom_background' );
+/**
+ * Setup the WordPress core custom background feature.
+ *
+ * @uses   add_theme_support()
+ * @since  1.1
+ */
+function follet_theme_support_custom_background() {
 	add_theme_support( 'custom-background', apply_filters(
 			'follet_custom_background_args', array(
 				'default-color' => 'FFFFFF',
@@ -49,22 +64,50 @@ function follet_do_theme_support() {
 			)
 		)
 	);
+}
+endif;
 
-	// Enable support for HTML5 markup.
+if ( ! function_exists( 'follet_theme_support_html5' ) ) :
+add_action( 'follet_theme_support', 'follet_theme_support_html5' );
+/**
+ * Enable support for HTML5 markup.
+ *
+ * @since 1.1
+ * @uses  add_theme_support()
+ */
+function follet_theme_support_html5() {
 	add_theme_support( 'html5', apply_filters( 'follet_html5_args', array(
 		'comment-list',
 		'search-form',
 		'comment-form',
 	) ) );
+}
+endif;
 
-	// Enable support for Bootstrap.
+if ( ! function_exists( 'follet_theme_support_bootstrap' ) ) :
+add_action( 'follet_theme_support', 'follet_theme_support_bootstrap' );
+/**
+ * Initialize default theme support.
+ *
+ * @uses   add_theme_support()
+ * @since  1.1
+ */
+function follet_theme_support_bootstrap() {
 	add_theme_support( 'bootstrap' );
+}
+endif;
 
-	/**
-	 * Add theme support for Infinite Scroll.
-	 *
-	 * @see {@link http://jetpack.me/support/infinite-scroll/}
-	 */
+if ( ! function_exists( 'follet_theme_support_infinite_scroll' ) ) :
+add_action( 'follet_theme_support', 'follet_theme_support_infinite_scroll' );
+/**
+ * Add theme support for Infinite Scroll.
+ *
+ * @link  http://jetpack.me/support/infinite-scroll/
+ *
+ * @uses  add_theme_support()
+ * @since 1.1
+ */
+function follet_theme_support_infinite_scroll() {
 	add_theme_support( 'infinite-scroll', apply_filters(
 			'follet_infinite_scroll', array(
 				'container' => 'main',
