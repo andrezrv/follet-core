2db997365c77cb7fe78ed2fb5c76248618b104e7
andrezrv <andrezrv@gmail.com>
2014-12-26 20:44:26 -0300

    Normalize text control class for customizer.

--- includes/customizer/class-follet-text-control.php
+++ includes/customizer/class-follet-text-control.php
@@ -1,20 +1,34 @@
 <?php
 /**
- * Follet_Plain_Text_Control
+ * Follet Core.
  *
  * Extend WP_Customize_Control to show text-based tips for controls.
  *
- * @package Follet_Core
- * @since   1.0
+ * @package   Follet_Core
+ * @author    Andrés Villarreal <andrezrv@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://github.com/andrezrv/follet-core
+ * @copyright 2014-2015 Andrés Villarreal
+ * @since     1.0
  */
-if ( class_exists( 'WP_Customize_Control' ) and ! class_exists( 'Follet_Plain_Text_Control' ) ) :
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Follet_Plain_Text_Control' ) ) :
 /**
- * Class to create a custom date picker
+ * Class Follet_Plain_Text_Control
+ *
+ * Class to create a custom date picker.
+ *
+ * @since 1.0
  */
 class Follet_Plain_Text_Control extends WP_Customize_Control {
     /**
-    * Render the content on the theme customizer page
-    */
+     * Render the content on the theme customizer page
+     *
+     * @since 1.0
+     */
     public function render_content() {
         echo $this->value();
     }
